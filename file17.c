b08dad50798d6a3d38d970e5cd5a7e5dfa8badf7
Suhani-Balooni <suhanibalooni06@gmail.com>
2026-02-17 06:52:31 +0000

    file17.c

---
+++ file17.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0], min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("\nMax: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}
